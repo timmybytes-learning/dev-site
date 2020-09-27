909789b497984424b5c4393eefc6989d91beaa46
Timothy Merritt <timmybytes@gmail.com>
2020-09-27 08:48:02 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Contents from "./Contents";
+
+function App() {
+  return (
+    <div className="">
+      <Contents />
+    </div>
+  );
+}
+
+export default App;
