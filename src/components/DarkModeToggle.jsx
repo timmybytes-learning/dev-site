0c6ee498dbdbd65154beafe5e3f8d9d4a4fae491
Timothy Merritt <timmybytes@gmail.com>
2020-09-29 06:30:12 -0700

    Add icons and dark mode toggle

---
+++ src/components/DarkModeToggle.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+
+export default function DarkModeToggle() {
+  // const checkboxToggle = document.getElementById("checkbox");
+
+  // checkboxToggle.addEventListener("changed", () => {
+  //   // change theme
+  // });
+
+  const handleClick = () => {
+    document.body.classList.toggle("dark");
+  };
+
+  return (
+    <div>
+      <input type="checkbox" className="checkbox" id="checkbox" />
+      <label htmlFor="checkbox" className="toggle-label" onClick={handleClick}>
+        <FaMoon className="toggle-icons" />
+        <FaSun className="toggle-icons" />
+        <div className="ball"></div>
+      </label>
+    </div>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import DarkModeToggle from "./DarkModeToggle";
 
 export default function Header() {
   return (
-    <header className="header border flex center-all">
-      <h1>DEV ROADMAP</h1>
-    </header>
+    <>
+      <header className="header border flex center-all">
+        <h1>DEV ROADMAP</h1>
+        <DarkModeToggle />
+      </header>
+    </>
   );
 }
